5f5acf5c4110230745007e1d359041eaa63ffaf8
kennroo <kennroo22@gmail.com>
2020-08-15 06:00:05 -0500

    Webkit is mean

--- server/static/assets/scripts/main.js
+++ server/static/assets/scripts/main.js
@@ -1136,87 +1136,96 @@ function handleNewPage(title, contentTag, call, vari) {
 }
 
 class Router {
-    routes = [];
+    /*constructor() {
+        routes = [];
 
-    mode = null;
+        mode = null;
 
-    root = '/';
+        root = '/';
+    }*/
 
     constructor(options) {
+        this.routes = [];
+
+        this.mode = null;
+
+        this.root = '/';
         this.mode = window.history.pushState ? 'history' : 'hash';
         if (options.mode) this.mode = options.mode;
         if (options.root) this.root = options.root;
+
+
+
+        this.add = (path, cb) => {
+            this.routes.push({ path, cb });
+            return this;
+        };
+
+        this.remove = path => {
+            for (let i = 0; i < this.routes.length; i += 1) {
+                if (this.routes[i].path === path) {
+                    this.routes.slice(i, 1);
+                    return this;
+                }
+            }
+            return this;
+        };
+
+        this.flush = () => {
+            this.routes = [];
+            return this;
+        };
+
+        this.clearSlashes = path =>
+            path
+            .toString();
+        //  .replace(/\/$/, '')
+        // .replace(/^\//, '');
+
+        this.getFragment = () => {
+            let fragment = '';
+            if (this.mode === 'history') {
+                fragment = this.clearSlashes(decodeURI(window.location.pathname + window.location.search));
+                console.log(fragment);
+                fragment = fragment.replace(/\?(.*)$/, '');
+                fragment = this.root !== '/' ? fragment.replace(this.root, '') : fragment;
+            } else {
+                const match = window.location.href.match(/(.*)$/);
+                fragment = match ? match[1] : '';
+            }
+            return this.clearSlashes(fragment);
+        };
+
+        this.navigate = (path = '') => {
+            if (this.mode === 'history') {
+                window.history.pushState(null, null, this.root + this.clearSlashes(path));
+            } else {
+                window.location.href = `${window.location.href.replace(/(.*)$/, '')}#${path}`;
+            }
+            return this;
+        };
+
+        this.listen = () => {
+            clearInterval(this.interval);
+            this.interval = setInterval(this.interval, 50);
+        };
+
+        this.interval = () => {
+            if (this.current === this.getFragment() || this.current + "#" === this.getFragment()) return;
+            this.current = this.getFragment();
+
+            this.routes.some(route => {
+                const match = this.current.match(route.path);
+                if (match) {
+                    match.shift();
+                    route.cb.apply({}, match);
+                    return match;
+                }
+                return false;
+            });
+        };
         this.listen();
     }
-
-    add = (path, cb) => {
-        this.routes.push({ path, cb });
-        return this;
-    };
-
-    remove = path => {
-        for (let i = 0; i < this.routes.length; i += 1) {
-            if (this.routes[i].path === path) {
-                this.routes.slice(i, 1);
-                return this;
-            }
-        }
-        return this;
-    };
-
-    flush = () => {
-        this.routes = [];
-        return this;
-    };
-
-    clearSlashes = path =>
-        path
-        .toString();
-    //  .replace(/\/$/, '')
-    // .replace(/^\//, '');
-
-    getFragment = () => {
-        let fragment = '';
-        if (this.mode === 'history') {
-            fragment = this.clearSlashes(decodeURI(window.location.pathname + window.location.search));
-            console.log(fragment);
-            fragment = fragment.replace(/\?(.*)$/, '');
-            fragment = this.root !== '/' ? fragment.replace(this.root, '') : fragment;
-        } else {
-            const match = window.location.href.match(/(.*)$/);
-            fragment = match ? match[1] : '';
-        }
-        return this.clearSlashes(fragment);
-    };
-
-    navigate = (path = '') => {
-        if (this.mode === 'history') {
-            window.history.pushState(null, null, this.root + this.clearSlashes(path));
-        } else {
-            window.location.href = `${window.location.href.replace(/(.*)$/, '')}#${path}`;
-        }
-        return this;
-    };
-
-    listen = () => {
-        clearInterval(this.interval);
-        this.interval = setInterval(this.interval, 50);
-    };
-
-    interval = () => {
-        if (this.current === this.getFragment() || this.current + "#" === this.getFragment()) return;
-        this.current = this.getFragment();
-
-        this.routes.some(route => {
-            const match = this.current.match(route.path);
-            if (match) {
-                match.shift();
-                route.cb.apply({}, match);
-                return match;
-            }
-            return false;
-        });
-    };
 }
 
 const router = new Router({
